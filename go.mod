dc0bc6ddb40ae852a3434e40268ee3db84d422a9
Steffen André Langnes <steffenl.sw@gmail.com>
2022-11-29 15:38:16 +0900

    Strip project and add example for issue 857

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/webview/webview
 
 go 1.13
+
+require github.com/abemedia/go-winsparkle v0.0.0-20200801031821-8b66f9113d7e // indirect
--- webview.go
+++ webview.go
@@ -8,23 +8,13 @@ package webview
 #cgo darwin LDFLAGS: -framework WebKit
 
 #cgo windows CXXFLAGS: -DWEBVIEW_EDGE -std=c++17
-#cgo windows LDFLAGS: -static -lWebView2Loader.dll -lole32 -lshell32 -lshlwapi -luser32
+#cgo windows LDFLAGS: -static -lole32 -lshell32 -lshlwapi -luser32
 
 #include "webview.h"
-
-#include <stdlib.h>
-#include <stdint.h>
-
-void CgoWebViewDispatch(webview_t w, uintptr_t arg);
-void CgoWebViewBind(webview_t w, const char *name, uintptr_t index);
 */
 import "C"
 import (
-	"encoding/json"
-	"errors"
-	"reflect"
 	"runtime"
-	"sync"
 	"unsafe"
 )
 
@@ -116,20 +106,6 @@ type webview struct {
 	w C.webview_t
 }
 
-var (
-	m        sync.Mutex
-	index    uintptr
-	dispatch = map[uintptr]func(){}
-	bindings = map[uintptr]func(id, req string) (interface{}, error){}
-)
-
-func boolToInt(b bool) C.int {
-	if b {
-		return 1
-	}
-	return 0
-}
-
 // New calls NewWindow to create a new window and a new webview instance. If debug
 // is non-zero - developer tools will be enabled (if the platform supports them).
 func New(debug bool) WebView { return NewWindow(debug, nil) }
@@ -142,174 +118,43 @@ func New(debug bool) WebView { return NewWindow(debug, nil) }
 // here. Returns nil on failure. Creation can fail for various reasons such as when
 // required runtime dependencies are missing or when window creation fails.
 func NewWindow(debug bool, window unsafe.Pointer) WebView {
-	res := C.webview_create(boolToInt(debug), window)
-	if res == nil {
-		return nil
-	}
-	return &webview{w: res}
+	return nil
 }
 
 func (w *webview) Destroy() {
-	C.webview_destroy(w.w)
 }
 
 func (w *webview) Run() {
-	C.webview_run(w.w)
 }
 
 func (w *webview) Terminate() {
-	C.webview_terminate(w.w)
 }
 
 func (w *webview) Window() unsafe.Pointer {
-	return C.webview_get_window(w.w)
+	return nil
 }
 
 func (w *webview) Navigate(url string) {
-	s := C.CString(url)
-	defer C.free(unsafe.Pointer(s))
-	C.webview_navigate(w.w, s)
 }
 
 func (w *webview) SetHtml(html string) {
-	s := C.CString(html)
-	defer C.free(unsafe.Pointer(s))
-	C.webview_set_html(w.w, s)
 }
 
 func (w *webview) SetTitle(title string) {
-	s := C.CString(title)
-	defer C.free(unsafe.Pointer(s))
-	C.webview_set_title(w.w, s)
 }
 
 func (w *webview) SetSize(width int, height int, hint Hint) {
-	C.webview_set_size(w.w, C.int(width), C.int(height), C.int(hint))
 }
 
 func (w *webview) Init(js string) {
-	s := C.CString(js)
-	defer C.free(unsafe.Pointer(s))
-	C.webview_init(w.w, s)
 }
 
 func (w *webview) Eval(js string) {
-	s := C.CString(js)
-	defer C.free(unsafe.Pointer(s))
-	C.webview_eval(w.w, s)
 }
 
 func (w *webview) Dispatch(f func()) {
-	m.Lock()
-	for ; dispatch[index] != nil; index++ {
-	}
-	dispatch[index] = f
-	m.Unlock()
-	C.CgoWebViewDispatch(w.w, C.uintptr_t(index))
-}
-
-//export _webviewDispatchGoCallback
-func _webviewDispatchGoCallback(index unsafe.Pointer) {
-	m.Lock()
-	f := dispatch[uintptr(index)]
-	delete(dispatch, uintptr(index))
-	m.Unlock()
-	f()
-}
-
-//export _webviewBindingGoCallback
-func _webviewBindingGoCallback(w C.webview_t, id *C.char, req *C.char, index uintptr) {
-	m.Lock()
-	f := bindings[uintptr(index)]
-	m.Unlock()
-	jsString := func(v interface{}) string { b, _ := json.Marshal(v); return string(b) }
-	status, result := 0, ""
-	if res, err := f(C.GoString(id), C.GoString(req)); err != nil {
-		status = -1
-		result = jsString(err.Error())
-	} else if b, err := json.Marshal(res); err != nil {
-		status = -1
-		result = jsString(err.Error())
-	} else {
-		status = 0
-		result = string(b)
-	}
-	s := C.CString(result)
-	defer C.free(unsafe.Pointer(s))
-	C.webview_return(w, id, C.int(status), s)
 }
 
 func (w *webview) Bind(name string, f interface{}) error {
-	v := reflect.ValueOf(f)
-	// f must be a function
-	if v.Kind() != reflect.Func {
-		return errors.New("only functions can be bound")
-	}
-	// f must return either value and error or just error
-	if n := v.Type().NumOut(); n > 2 {
-		return errors.New("function may only return a value or a value+error")
-	}
-
-	binding := func(id, req string) (interface{}, error) {
-		raw := []json.RawMessage{}
-		if err := json.Unmarshal([]byte(req), &raw); err != nil {
-			return nil, err
-		}
-
-		isVariadic := v.Type().IsVariadic()
-		numIn := v.Type().NumIn()
-		if (isVariadic && len(raw) < numIn-1) || (!isVariadic && len(raw) != numIn) {
-			return nil, errors.New("function arguments mismatch")
-		}
-		args := []reflect.Value{}
-		for i := range raw {
-			var arg reflect.Value
-			if isVariadic && i >= numIn-1 {
-				arg = reflect.New(v.Type().In(numIn - 1).Elem())
-			} else {
-				arg = reflect.New(v.Type().In(i))
-			}
-			if err := json.Unmarshal(raw[i], arg.Interface()); err != nil {
-				return nil, err
-			}
-			args = append(args, arg.Elem())
-		}
-		errorType := reflect.TypeOf((*error)(nil)).Elem()
-		res := v.Call(args)
-		switch len(res) {
-		case 0:
-			// No results from the function, just return nil
-			return nil, nil
-		case 1:
-			// One result may be a value, or an error
-			if res[0].Type().Implements(errorType) {
-				if res[0].Interface() != nil {
-					return nil, res[0].Interface().(error)
-				}
-				return nil, nil
-			}
-			return res[0].Interface(), nil
-		case 2:
-			// Two results: first one is value, second is error
-			if !res[1].Type().Implements(errorType) {
-				return nil, errors.New("second return value must be an error")
-			}
-			if res[1].Interface() == nil {
-				return res[0].Interface(), nil
-			}
-			return res[0].Interface(), res[1].Interface().(error)
-		default:
-			return nil, errors.New("unexpected number of return values")
-		}
-	}
-
-	m.Lock()
-	for ; bindings[index] != nil; index++ {
-	}
-	bindings[index] = binding
-	m.Unlock()
-	cname := C.CString(name)
-	defer C.free(unsafe.Pointer(cname))
-	C.CgoWebViewBind(w.w, cname, C.uintptr_t(index))
 	return nil
 }
